bcb535d7c47c0f4fed22c839e108c0fb5ed9ad02
DLois97 <danielloisnuevo@gmail.com>
2020-10-26 22:34:16 +0100

    cambios de estilo

--- app/src/main/java/com/example/quiz/Activity2.java
+++ app/src/main/java/com/example/quiz/Activity2.java
@@ -6,15 +6,15 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
-
+    private int puntuacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
         Bundle datos = this.getIntent().getExtras();
-        int var_puntuacion = datos.getInt("puntuacion");
+        puntuacion = datos.getInt("puntuacion");
         TextView tv = (TextView) findViewById(R.id.Puntuacion);
-        tv.setText("Puntuacion:"+var_puntuacion );
+        tv.setText("Puntuacion:"+this.puntuacion);
 
     }
 }
