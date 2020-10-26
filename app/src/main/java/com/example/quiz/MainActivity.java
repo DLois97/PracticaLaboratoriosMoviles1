5fd4e3e97a7b215b67a6d61cc39a03fd915ed183
DLois97 <danielloisnuevo@gmail.com>
2020-10-26 22:34:09 +0100

    cambios de estilo

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -10,24 +10,24 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-        int puntuacion = 0;
+    private int puntuacion = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void Respuesta(View view){
-            RadioButton idR = (RadioButton) findViewById(R.id.RespuestaCorrecta1);
-            if(idR.isChecked()) {
-                puntuacion += 3;
-                Toast.makeText(this,"Respuesta correcta",Toast.LENGTH_LONG).show();
-            }else{
-                Toast.makeText(this,"Respuesta incorrecta ",Toast.LENGTH_LONG).show();
-                puntuacion-=2;
+        RadioButton idR = (RadioButton) findViewById(R.id.RespuestaCorrecta1);
+        if(idR.isChecked()) {
+            puntuacion += 3;
+            Toast.makeText(this,"Respuesta correcta",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Respuesta incorrecta ",Toast.LENGTH_LONG).show();
+            puntuacion-=2;
         }
         Intent intent = new Intent(this, Activity2.class);
         intent.putExtra("puntuacion",getPuntuacion());
-            startActivity(intent);
+        startActivity(intent);
 
 
     }
