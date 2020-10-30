a08f8540a1de731ffd8e46deb753ad82002e43f9
GuileYoyo <guillermoyoyo@gmail.com>
2020-10-30 18:52:08 +0100

    Intento de añadido de ListView en la activity_main_third_question.xml , la app peta

--- app/src/main/java/com/example/quiz/MainActivityThirdQuestion.java
+++ app/src/main/java/com/example/quiz/MainActivityThirdQuestion.java
@@ -3,11 +3,20 @@ package com.example.quiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivityThirdQuestion extends AppCompatActivity {
 
     private int puntuacion;
+    private ListView lista;
+    private ArrayList<String> respuestas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +26,27 @@ public class MainActivityThirdQuestion extends AppCompatActivity {
         puntuacion = datos.getInt("puntuacion");
         TextView tv = (TextView) findViewById(R.id.puntuacion3);
         tv.setText("Puntuacion:"+this.puntuacion);
+
+
+        lista = findViewById(R.id.ListaRespuestas);
+
+        respuestas = new ArrayList<>();
+        respuestas.add("8");
+        respuestas.add("7");
+        respuestas.add("6");
+        respuestas.add("3");
+
+        ArrayAdapter adaptadorRespuestas = new ArrayAdapter(this,android.R.layout.simple_list_item_1,respuestas);
+
+        lista.setAdapter(adaptadorRespuestas);
+
+        lista.setOnItemClickListener((AdapterView.OnItemClickListener) this);
+    }
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+        String respuestasCadena = respuestas.get(position);
+        Log.i("RESPUESTA","Respuesta seleccionada:" + respuestas);
     }
 
     public int getPuntuacion() {
