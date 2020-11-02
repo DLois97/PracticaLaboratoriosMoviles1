c7e9544d5a6ad7e1c28402ae65e9f6c4b27535cf
GuileYoyo <guillermoyoyo@gmail.com>
2020-11-02 14:44:37 +0100

    Añadida primera pantalla para empezar el juego

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -11,31 +11,14 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private int puntuacion = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void Respuesta(View view){
-        RadioButton idR = (RadioButton) findViewById(R.id.RespuestaCorrecta1);
-        if(idR.isChecked()) {
-            puntuacion += 3;
-            Toast.makeText(this,"Respuesta correcta",Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this,"Respuesta incorrecta ",Toast.LENGTH_LONG).show();
-            if(puntuacion<=2){
-                puntuacion=0;
-            }else {
-                puntuacion -= 2;
-            }
-        }
-        Intent intent = new Intent(this, Activity2.class);
-        intent.putExtra("puntuacion",getPuntuacion());
+    public void empezar(View view){
+        Intent intent = new Intent(this, MainActivityFirstQuestion.class);
         startActivity(intent);
-
-
-    }
-    public int getPuntuacion(){
-        return puntuacion;
     }
 }
---
+++ app/src/main/java/com/example/quiz/MainActivityFirstQuestion.java
@@ -0,0 +1,40 @@
+package com.example.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class MainActivityFirstQuestion extends AppCompatActivity {
+    private int puntuacion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_first_question);
+    }
+    public void Respuesta(View view){
+        RadioButton idR = (RadioButton) findViewById(R.id.RespuestaCorrecta1);
+        if(idR.isChecked()) {
+            puntuacion += 3;
+            Toast.makeText(this,"Respuesta correcta",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this,"Respuesta incorrecta ",Toast.LENGTH_LONG).show();
+            if(puntuacion<=2){
+                puntuacion=0;
+            }else {
+                puntuacion -= 2;
+            }
+        }
+        Intent intent = new Intent(this, Activity2.class);
+        intent.putExtra("puntuacion",getPuntuacion());
+        startActivity(intent);
+
+
+    }
+    public int getPuntuacion(){
+        return puntuacion;
+    }
+}
